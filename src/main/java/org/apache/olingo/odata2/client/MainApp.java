8f42079b4981aa9b4e8497fe71b9b6565a827475
Michael <michael@mirb.de>
2013-09-12 08:19:00 +0200

    Added properties, resource filterting and Logging

--- src/main/java/org/apache/olingo/odata2/client/MainApp.java
+++ src/main/java/org/apache/olingo/odata2/client/MainApp.java
@@ -1,24 +1,45 @@
 package org.apache.olingo.odata2.client;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MainApp extends Application {
 
+  private static final Logger LOG = LoggerFactory.getLogger(MainApp.class);
+  private final Properties properties;
+  
   public static void main(String[] args) throws Exception {
     launch(args);
   }
 
+  public MainApp() {
+    properties = new Properties();
+    
+    try (InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("client.properties");) {
+      properties.load(inputStream);
+    } catch (IOException e) {
+      LOG.error("Failure during properties initialization with message: {}.", e.getMessage());
+    }
+  }
+
+  
+  
   @Override
   public void start(Stage stage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("fxml/Basic.fxml"));
 
     Scene scene = new Scene(root);
 
-    stage.setTitle("Olingo Sample Client - OData-v2");
+    String buildVersion = properties.getProperty("build.version");
+    stage.setTitle("Olingo Sample Client - OData-v2 - " + buildVersion);
     stage.setScene(scene);
     stage.show();
   }
